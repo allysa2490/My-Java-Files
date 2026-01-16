89eea30ed9e5034002be7c33ad1f358d626985e6
khianvictorycalderon <allysacastro57@gmail.com>
2026-01-16 16:09:25 +0800

    Added concatenation

---
+++ Concatenation.java
@@ -0,0 +1,11 @@
+public class Concatenation {
+    public static void main(String[] args) {
+
+        // String concatenation
+        String name = "Lysa";
+        int age = 23;
+
+        System.out.println("Hi, I am " + name + ", " + age + " years old.");
+
+    }
+}

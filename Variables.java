43305ef965d9591b99f3caa796d4b2e0b6862de7
khianvictorycalderon <allysacastro57@gmail.com>
2026-01-16 15:57:31 +0800

    Added variables file

---
+++ Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        // Tyes of variables
+        int A = 15;
+        float B = 35.5f;
+        char SingleCharacter = 'C';
+        String text = "This is a sentence";
+
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(SingleCharacter);
+        System.out.println(text);
+
+    }
+}

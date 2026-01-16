0152d1c2409e62d4859a93a3a1db7d89d3cd28ce
khianvictorycalderon <allysacastro57@gmail.com>
2026-01-16 16:42:49 +0800

    Added first object-oriented programming file

---
+++ WalletObject.java
@@ -0,0 +1,26 @@
+// This is the object
+class Wallet {
+
+    String owner;
+    int price;
+
+    // Constructor
+    Wallet(String owner, int price) {
+        this.owner = owner;
+        this.price = price;
+    }
+
+}
+
+public class WalletObject {
+    public static void main(String[] args) {
+        
+        Wallet cab = new Wallet("Khian", 200);
+
+        System.out.println("Owner of Wallet: ");
+        System.out.println(cab.owner);
+        System.out.println("Price of Wallet: ");
+        System.out.println(cab.price);
+        
+    }
+}
